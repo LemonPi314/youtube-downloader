0e383c6174239c53071c8a816cd872dd28e413f3
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2021-06-09 16:16:02 -0700

    flake8 formatting

--- readme.md
+++ readme.md
@@ -15,7 +15,7 @@ Optional executable file for Windows users. Python and the required packages are
 - 70 MB of free space for temporary files
 - Internet connection
 ## Usage
-Paste the URL of the video into the URL field, select an output folder, and click 'Get Info'. After a few seconds a list of available streams will appear. Select the streams you would like to download and click 'Download'.
+Paste the URL of the video into the URL field, select an output folder, and click "Get Info". After a few seconds a list of available streams will appear. Select the streams you would like to download and click "Download".
 
 Some streams have both video and audio tracks. However, some streams have only the video track or only the audio track, which will download as separate files. You can combine video and audio tracks using a tool such as [FFmpeg](https://ffmpeg.org/).  
 To play some streams you will need to install a codec extension such as the AV1 Video Extension from the Microsoft Store on Windows.
--- youtube-downloader.py
+++ youtube-downloader.py
@@ -7,30 +7,31 @@ sg.theme('Black')
 layout = [
     [
         sg.Column([
-            [sg.Text("Video URL", key='url_title')], 
-            [sg.Input(key='url', size=(54, 1))], 
-            [sg.Text(key='url_text', size=(40, 2))], 
-            [sg.Text("Output folder", key='output_dir_title')], 
-            [sg.Input(key='output_dir'), sg.FolderBrowse()], 
-            [sg.Text(key='output_dir_text', size=(40, 2))], 
-            [sg.Button("Get info", key='next_button')], 
-            [sg.Text("Info", key='info_title')], 
+            [sg.Text("Video URL", key='url_title')],
+            [sg.Input(key='url', size=(54, 1))],
+            [sg.Text(key='url_text', size=(40, 2))],
+            [sg.Text("Output folder", key='output_dir_title')],
+            [sg.Input(key='output_dir'), sg.FolderBrowse()],
+            [sg.Text(key='output_dir_text', size=(40, 2))],
+            [sg.Button("Get info", key='next_button')],
+            [sg.Text("Info", key='info_title')],
             [sg.Multiline(key='video_info', size=(54, 5), disabled=True, no_scrollbar=True)]
         ], vertical_alignment='top'),
         sg.VerticalSeparator(),
         sg.Column([
-            [sg.Text("Streams", key='stream_list_title')], 
-            [sg.Listbox(values=[], key='stream_list', size=(54, 16), enable_events=True, select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE)], 
+            [sg.Text("Streams", key='stream_list_title')],
+            [sg.Listbox(values=[], key='stream_list', size=(54, 16), enable_events=True, select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE)],
             [sg.Button("Download", key='download_button', disabled=True)]
         ], vertical_alignment='top')
     ],
     [
         sg.Column([
-            [sg.Text(key='progress_text', size=(80, 1))], 
+            [sg.Text(key='progress_text', size=(80, 1))],
             [sg.ProgressBar(max_value=1000, key='progress_bar', size=(74, 20))]
         ], justification='center')
     ]
 ]
+
 
 def ProgressUpdate(stream, chunk, bytesRemaining):
     totalSize = stream.filesize
@@ -38,6 +39,7 @@ def ProgressUpdate(stream, chunk, bytesRemaining):
     percent = round(bytesDone / totalSize * 100)
     window['progress_text'].update(f"Downloading... {percent}% ({currentItem}/{items})", text_color='white')
     progressBar.update(bytesDone, totalSize)
+
 
 def FormatStreams(streams: list[Stream]):
     formattedStreams = []
@@ -58,7 +60,7 @@ def FormatStreams(streams: list[Stream]):
                 formattedStream.append(str(round(stream.filesize / (1024 ** 3), 2)) + " GB")
 
             formattedStream.append(stream.video_codec)
-        
+
             if stream.includes_audio_track:
                 formattedStream.insert(1, "Audio")
 
@@ -78,8 +80,9 @@ def FormatStreams(streams: list[Stream]):
             formattedStream.append(stream.audio_codec)
 
         formattedStreams.append(', '.join(formattedStream))
-    
+
     return formattedStreams
+
 
 window = sg.Window("YouTube Downloader", layout)
 progressBar = window['progress_bar']
@@ -105,11 +108,11 @@ while True:
                 window.refresh()
                 video = YouTube(values['url'], on_progress_callback=ProgressUpdate)
                 window['video_info'].update(
-                    f"Title: {str(video.title)}\n" +
-                    f"Author: {str(video.author)}\n" +
-                    f"Views: {str(video.views)}\n" +
-                    f"Length: {str(video.length)}\n" +
-                    f"Published: {str(video.publish_date)}"
+                    f"Title: {str(video.title)}\n"
+                    + f"Author: {str(video.author)}\n"
+                    + f"Views: {str(video.views)}\n"
+                    + f"Length: {str(video.length)}\n"
+                    + f"Published: {str(video.publish_date)}"
                 )
                 formattedStreams = FormatStreams(video.streams)
                 window['stream_list'].update(formattedStreams)
@@ -129,12 +132,12 @@ while True:
                 videoStream = video.streams[index]
                 if path.exists(f"{values['output_dir']}/{videoStream.default_filename}"):
                     prefix = str(currentItem) + '-'
-                    
+
                 videoStream.download(values['output_dir'], None, prefix)
 
             window['progress_text'].update(f"Downloaded 100% ({items}/{items})", text_color='white')
 
-        except:
+        except Exception:
             window['progress_text'].update("Failed to download", text_color='red')
 
     if values['stream_list'] != []:
